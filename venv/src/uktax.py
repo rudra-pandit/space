8ac9ec0bba813ce3a87edd4f9ea0724ff39b2495
Rudra <rudra.h.pandit@outlook.com>
2022-05-07 08:39:12 +0100

    added from old work

--- venv/src/uktax.py
+++ venv/src/uktax.py
@@ -1,7 +1,7 @@
 a = int(input("please enter your salary"))
-if (a <= 37701):
-  print("you are in the basic rate")
-elif a<150000:
-  print("you are in the higher rate")
+if a <= 37701:
+    print("you are in the basic rate")
+elif a < 150000:
+    print("you are in the higher rate")
 else:
-  print("you are in the highest rate")
+    print("you are in the highest rate")
